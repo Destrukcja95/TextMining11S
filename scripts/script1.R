f1a78b29202254dfede1a3caa41be385cf5e9080
Fiona84 <fiona_17@interia.pl>
2020-03-24 12:53:34 +0100

    KW 2020-03-24

--- scripts/script1.R
+++ scripts/script1.R
@@ -1,8 +1,11 @@
 #załadowanie bibliotek
 library(tm)
+library(hunspell)
+library(stringr)
 
 #zmiana katalogu roboczego
-workDir <- "D:\\KW\\TextMining11S"
+#workDir <- "F:/KW/TextMining11S"
+workDir <- "F:\\KW\\TextMining11S"
 setwd(workDir)
 
 #definicja katalogów funkcjonalnych
@@ -42,6 +45,42 @@ stoplistFile <- paste(
 stoplist <- readLines(stoplistFile, encoding = "UTF-8")
 corpus <- tm_map(corpus, removeWords, stoplist)
 corpus <- tm_map(corpus, stripWhitespace)
+
+#usunięcie em dash i 3/4
+removeChar <- content_transformer(function(x,pattern) gsub(pattern, "", x))
+corpus <- tm_map(corpus, removeChar, intToUtf8(8722))
+corpus <- tm_map(corpus, removeChar, intToUtf8(190))
+
+#lematyzacja
+polish <- dictionary(lang="pl_PL")
+lemmatize <- function(text) {
+  simpleText <- str_trim(as.character(text))
+  parsedText <- strsplit(simpleText, split = " ")
+  newTextVec <- hunspell_stem(parsedText[[1]], dict = polish)
+  for (i in 1:length(newTextVec)) {
+    if (length(newTextVec[[i]]) == 0) newTextVec[i] <- parsedText[[1]][i]
+    if (length(newTextVec[[i]]) > 1) newTextVec[i] <- newTextVec[[i]][1]
+  }
+  newText <- paste(newTextVec, collapse = " ")
+  return(newText)
+}
+corpus <- tm_map(corpus, content_transformer(lemmatize))
+
+#usunięcie rozszerzeń z nazw plików
+cutExtensions <- function(document){
+  meta(document, "id") <- gsub(pattern = "\\.txt$", replacement = "", meta(document, "id"))
+  return(document)
+}
+corpus <- tm_map(corpus, cutExtensions)
+
+#eksport zawartości korpusu do plików tekstowych
+preprocessedDir <- paste(
+  outputDir, 
+  "Literatura - streszczenia - przeworzone",
+  sep = "\\"
+)
+dir.create(preprocessedDir)
+writeCorpus(corpus, path = preprocessedDir)
 
 #wyświetlenie zawartości pojeedynczego dokumentu
 writeLines(as.character(corpus[[1]]))
